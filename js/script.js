e5054085e10b8eff8d449d045b52ce6fc98b6cd4
linjojesan <urielpro78@gmail.com>
2017-03-01 14:17:01 -0800

    still trying to figure the right jquery function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
 $(document).ready(function() {
-$(".radio1, .radio").click(function() {
-  $("#btn btn-default").submit(function(event)
-});
-var climate = $("input:radio[name=climate]:checked").val();
-var kind = $("input:radio[name=kind]:checked").val();
-var stay = $("input:radio[name=stay]:checked").val();
-var long = $("input:radio[name=long]:checked").val();
-var continent = $("input:radio[name=continent]:checked").val();
+  $(".btn btn-default").click(function(event) {
+
+      var climate = $("input:radio[name=climate]:checked").val();
+      var kind = $("input:radio[name=kind]:checked").val();
+      var stay = $("input:radio[name=stay]:checked").val();
+      var long = $("input:radio[name=long]:checked").val();
+      var continent = $("input:radio[name=continent]:checked").val();
+      console.log(climate).click(".btn btn-default");
+          event.preventDefault();
 
 // if (climate === "hot") {
-//   $("#j1").show();
-
-event.preventDefault();
-// console.log(climate);
+//   return destination1
+//
+//
+// }
 });
 
-
-
-
-//
-/
-//
-//
-//
-// event.preventDefault();
+// console.log(climate);
+});
