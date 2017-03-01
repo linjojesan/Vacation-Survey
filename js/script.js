bd6c7da82bacb824def08b9145a1fc07881c2a9b
linjojesan <urielpro78@gmail.com>
2017-03-01 12:54:45 -0800

    jquery functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
 $(".radio1, .radio").click(function() {
+  $("#btn btn-default").submit(function(event)
+});
 var climate = $("input:radio[name=climate]:checked").val();
 var kind = $("input:radio[name=kind]:checked").val();
 var stay = $("input:radio[name=stay]:checked").val();
 var long = $("input:radio[name=long]:checked").val();
 var continent = $("input:radio[name=continent]:checked").val();
-console.log(stay);
 
+// if (climate === "hot") {
+//   $("#j1").show();
 
 event.preventDefault();
+// console.log(climate);
 });
-});
+
+
+
+
+//
+/
+//
+//
+//
+// event.preventDefault();
