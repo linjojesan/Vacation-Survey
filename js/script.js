c6b567b0093c135c8e45a114f643b40ec32a68c5
linjojesan <urielpro78@gmail.com>
2017-03-01 11:48:11 -0800

    created jquery click functions and radio input variables

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+$(document).ready(function() {
+$(".radio1, .radio").click(function() {
+var climate = $("input:radio[name=climate]:checked").val();
+var kind = $("input:radio[name=kind]:checked").val();
+var stay = $("input:radio[name=stay]:checked").val();
+var long = $("input:radio[name=long]:checked").val();
+var continent = $("input:radio[name=continent]:checked").val();
+console.log(stay);
 
 
-  var destination = document.getElementsByClassName("radio1");
-    if ("destination.checked"); {
-      alert("radio1 selected"); }
-      event.preventDefault();
+event.preventDefault();
+});
+});
