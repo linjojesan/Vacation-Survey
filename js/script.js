a38b7179c8cc8a67262232febdee588202764347
John <urielpro78@gmail.com>
2017-03-01 23:22:43 -0800

    figured out functions, cleaned up code, changed values to numbers

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 $(document).ready(function() {
-  $(".btn btn-default").click(function(event) {
+  $("form#survey").submit(function(event) {
 
-      var climate = $("input:radio[name=climate]:checked").val();
-      var kind = $("input:radio[name=kind]:checked").val();
-      var stay = $("input:radio[name=stay]:checked").val();
-      var long = $("input:radio[name=long]:checked").val();
-      var continent = $("input:radio[name=continent]:checked").val();
-      console.log(climate).click(".btn btn-default");
-          event.preventDefault();
+    var climate = parseInt($("input:radio[name=climate]:checked").val());
+    var kind = parseInt($("input:radio[name=kind]:checked").val());
+    var stay = parseInt($("input:radio[name=stay]:checked").val());
+    var long = parseInt($("input:radio[name=long]:checked").val());
+    var continent = parseInt($("input:radio[name=continent]:checked").val());
+    console.log(climate);
+    console.log(kind);
+    console.log(stay);
+    console.log(long);
+    console.log(continent);
 
-// if (climate === "hot") {
-//   return destination1
-//
-//
+
+
+
+
+
+
+    if (climate === 1 ) {
+     $("#bangkok").show();
+     }
 // }
-});
+
+//
+//
+    event.preventDefault();
+  });
 
 // console.log(climate);
 });
